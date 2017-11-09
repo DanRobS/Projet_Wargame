e482548cbde07de6bb0ae90925adbe9e920e9888
DanRobS <roberttsoumbou@gmail.com>
2017-11-09 16:24:37 +0100

    Add files via upload

---
+++ Soldat.java
@@ -0,0 +1,43 @@
+package wargame;
+
+public class Soldat implements ISoldat{
+
+	
+	
+	@Override
+	public int getPoints() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getTour() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getPortee() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void joueTour(int tour) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void combat(Soldat soldat) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void seDeplace(Position newPos) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
