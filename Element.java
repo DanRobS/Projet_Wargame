58bbf62d95a51a5d5292fa6ae1a622bdf61594b7
mohamedhmh <habibhabibmohamed95@gmail.com>
2017-11-14 23:17:17 +0100

    Classe Element et Soldat, methodes Soldat pas encore définis

--- Element.java
+++ Element.java
@@ -1,5 +1,72 @@
 package wargame;
 
-public abstract class Element {
+import java.awt.Color;
+
+/**
+ * Classe element qui renvoie et modife
+ * La position la couleur et la visibiliter
+ * 
+ */
+
+public abstract class Element implements IConfig {
+
+	protected Position position;
+	protected boolean elementVisible;
+	protected Color couleur;
+	
+	/******* les getters *******/
+	
+	/**
+	 * getters de position, couleur et elementVisible
+	 * @return position, couleur et elelntVisible 
+	 */
+	
+	public Position getPosition() {
+		return this.position;
+	}
+	
+	public Color getCouleur() {
+		return this.couleur;
+	}
+	
+	public boolean getElementVisible() {
+		return this.elementVisible;
+	}
+	
+	/******* Les setters *******/
+	
+	/**
+	 * setters de position, couleur et elementVisible
+	 * @param position, couleur et elementVisible
+	 */
+	
+	public void setPosition(Position position) {
+		this.position=position;
+	}
+	
+	public void setColor(Color couleur) {
+		this.couleur=couleur;
+	}
+	
+	public void setElementVisible(boolean elementVisible) {
+		this.elementVisible=elementVisible;
+	}
+	
+	/******* toString peut etre utile pour decrire l'element  *******/
+	
+	/**
+	 * Class toString pour la description de l'elemnt
+	 * @return Chaine de character décrit l'element
+	 */
+	
+	public String toString() {
+		String s = "Element";
+		if(elementVisible)
+			return s+=""+this.getClass().getSimpleName();
+		else
+			return s+=" invisible";
+		
+	}
+
 
 }
--- Soldat.java
+++ Soldat.java
@@ -1,25 +1,119 @@
 package wargame;
 
-public class Soldat implements ISoldat{
 
+/**
+ * Classe soldat
+ * 
+ *
+ */
+
+public class Soldat extends Element implements ISoldat{
+	protected final char MONSTRE = 'M', HERO = 'H';/*Type de soldat*/
+	protected int nbrPointsVie;
+	protected final int PORTEE_VISUELLE;
+	protected final int PUISSANCE_FRAPPE;
+	protected final int PUISSANCE_TIR;
+	protected final int NBR_POINTS_VIE_MAX;
+	protected boolean enRepos = false;
+	protected boolean estMort = false;
+	protected int aJouer = 0;/*int car le prof à fait int*/
 	
+	/**
+	 * Costructor soldat
+	 * 
+	 * @param posSoldat
+	 * @param porteeVisuelle
+	 * @param puissanceFrappe
+	 * @param puissanceTir
+	 * @param nbrPointsVieMax
+	 */
 	
+	public Soldat(Position posSoldat, int porteeVisuelle, int puissanceFrappe, int puissanceTir,int nbrPointsVieMax ) {
+		
+		super(); /* super d'Element */
+		this.position=posSoldat;
+		this.PORTEE_VISUELLE=porteeVisuelle;
+		this.PUISSANCE_FRAPPE=puissanceFrappe;
+		this.PUISSANCE_TIR=puissanceTir;
+		this.NBR_POINTS_VIE_MAX=nbrPointsVieMax;
+		
+	}
+
+
+	/**
+	 * Les Getters et le setters de soldat
+	 * 
+	 */
+	
+	public int getNbrPointsVie() {
+		return nbrPointsVie;
+	}
+
+
+
+	public void setNbrPointsVie(int nbrPointsVie) {
+		this.nbrPointsVie = nbrPointsVie;
+	}
+
+
+
+	public boolean isEnRepos() {
+		return enRepos;
+	}
+
+
+
+	public void setEnRepos(boolean enRepos) {
+		this.enRepos = enRepos;
+	}
+
+
+
+	public boolean isEstMort() {
+		return estMort;
+	}
+
+
+
+	public void setEstMort(boolean estMort) {
+		this.estMort = estMort;
+	}
+
+
+
+	public int getPORTEE_VISUELLE() {
+		return PORTEE_VISUELLE;
+	}
+
+
+
+	public int getPUISSANCE_FRAPPE() {
+		return PUISSANCE_FRAPPE;
+	}
+
+
+
+	public int getPUISSANCE_TIR() {
+		return PUISSANCE_TIR;
+	}
+
+	/**
+	 * getters et methode pour isoldat
+	 */
+
 	@Override
 	public int getPoints() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.nbrPointsVie;
 	}
 
 	@Override
 	public int getTour() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.aJouer;
 	}
 
 	@Override
 	public int getPortee() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.PORTEE_VISUELLE;
 	}
 
 	@Override
@@ -37,6 +131,22 @@ public class Soldat implements ISoldat{
 	@Override
 	public void seDeplace(Position newPos) {
 		// TODO Auto-generated method stub
+		
+	}
+
+	/****** toString *******/
+	
+	/**
+	 *Class toString pour la description du soldat
+	 * @return Chaine de character décrit l'element
+	 */
+	
+	public String toString() {
+		String s = "Soldat";
+		if(elementVisible)
+			return s+="Points de vie"+this.nbrPointsVie+" "+this.getClass().getSimpleName();
+		else
+			return s+=" invisible";
 		
 	}
 
